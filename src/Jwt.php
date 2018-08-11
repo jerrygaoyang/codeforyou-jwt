aa9be038ff4d285d53e299c2d0d99a8642ba5307
高杨 <gaoyang@gaoyangdeMacBook-Pro.local>
2018-08-11 14:36:31 +0800

    udpate

---
+++ src/Jwt.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gaoyang
+ * Date: 2018/8/11
+ * Time: 上午11:26
+ */
+
+namespace codeforyou\jwt;
+
+use codeforyou\jwt\exceptions\TokenException;
+
+class Jwt
+{
+    public static function header()
+    {
+        return [
+            'typ' => 'jwt'
+        ];
+    }
+
+    public static function safe_base64_decode($string)
+    {
+        $data = str_replace(array('-', '_'), array('+', '/'), $string);
+        $mod4 = strlen($data) % 4;
+        if ($mod4) {
+            $len = 4 - $mod4;
+            $data .= str_repeat('=', $len);
+        }
+        return base64_decode($data);
+    }
+
+
+    public static function safe_base64_encode($string)
+    {
+        $data = base64_encode($string);
+        $data = str_replace(array('+', '/', '='), array('-', '_', ''), $data);
+        return $data;
+    }
+
+    public static function sinature(array $header, array $payload, string $secret, string $alg)
+    {
+        $base64_header = self::safe_base64_encode(json_encode($header));
+        $base64_payload = self::safe_base64_encode(json_encode($payload));
+        $signature = hash_hmac($alg, $base64_header . $base64_payload, $secret, true);
+        $base64_signature = self::safe_base64_encode($signature);
+        return $base64_signature;
+    }
+
+    public static function encode(array $payload, string $secret, string $alg = 'sha256')
+    {
+        $header = self::header();
+        $header['alg'] = $alg;
+        $base64_header = self::safe_base64_encode(json_encode($header));
+        $base64_payload = self::safe_base64_encode(json_encode($payload));
+        $base64_signature = self::sinature($header, $payload, $secret, $alg);
+        $token = $base64_header . '.' . $base64_payload . '.' . $base64_signature;
+        return $token;
+    }
+
+    public static function decode(string $token, string $secret, string $alg = 'sha256')
+    {
+        $arr = explode('.', $token);
+        if (count($arr) != 3) {
+            throw new TokenException('invalid token');
+        }
+        $header = json_decode(self::safe_base64_decode($arr[0]));
+        $payload = json_decode(self::safe_base64_decode($arr[1]));
+        $base64_signature = self::sinature($header, $payload, $secret, $alg);
+        if (!hash_equals($base64_signature, $arr[2])) {
+            throw new TokenException('invalid token');
+        }
+        return $payload;
+    }
+
+}
---
+++ src/JwtException.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gaoyang
+ * Date: 2018/8/11
+ * Time: 下午1:05
+ */
+
+namespace codeforyou\jwt;
+
+use Exception;
+
+class JwtException extends Exception
+{
+
+}
